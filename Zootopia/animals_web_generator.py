355af1116b7d5f6c24b94d68e6502c1c318a6218
Walter <w.estebancr@gmail.com>
2024-07-03 18:47:00 +0200

    bring data to html

--- Zootopia/animals_web_generator.py
+++ Zootopia/animals_web_generator.py
@@ -7,15 +7,23 @@ def load_data(file_path):
 
 animals_data = load_data("animals_data.json")
 
+output = ""
 for fox_info_dictionary in animals_data:
     name = fox_info_dictionary["name"]
     diet = fox_info_dictionary["characteristics"]["diet"]
     first_location = fox_info_dictionary["locations"][0]
-    print(f"Name: {name}\nDiet: {diet}\nLocation: {first_location}")
+    output += f"\n\nName: {name}\nDiet: {diet}\nLocation: {first_location}"
     if "type" in fox_info_dictionary["characteristics"]:
         fox_type = fox_info_dictionary["characteristics"]["type"]
-        print(f"Type: {fox_type}")
-    print("")
+        output += f"Type: {fox_type}"
+#print(output)
+
+with open("animals_template.html", "r") as data:
+    animals_html = data.read()
+    animals_html = animals_html.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open("animals.html", "w") as new_html:
+    new_html.write(animals_html)
 
 
 

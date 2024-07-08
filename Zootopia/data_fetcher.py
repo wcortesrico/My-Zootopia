066d85db00f1bb2c8baaecf85531f3a07d65be58
Walter <w.estebancr@gmail.com>
2024-07-08 14:37:34 +0200

    env file added

--- Zootopia/data_fetcher.py
+++ Zootopia/data_fetcher.py
@@ -1,8 +1,11 @@
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 def fetch_data(animal_name):
-    API_key = "VtRld3UBsabpn8hSPFL+LA==0LXUcUl6h8bwBRUI"
     api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(animal_name)
-    response = requests.get(api_url, headers={'X-Api-Key': API_key})
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
     if response.status_code == requests.codes.ok:
         return (response.text)
     else:
